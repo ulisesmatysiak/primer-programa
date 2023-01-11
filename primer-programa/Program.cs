f30fbe0af1b7663d84c209d5ed001b10c6f02df8
ulises matysiak <ulisesmatysiak2gmail.com>
2023-01-11 20:43:55 -0300

    final commit

---
+++ primer-programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primer_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Uli!");
+        }
+    }
+}
